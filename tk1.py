327a0497ba355de3537007c21e2426ea3528b131
lassandra85 <lassandra85@gmail.com>
2024-10-27 19:03:02 +0100

    1

---
+++ tk1.py
@@ -0,0 +1,6 @@
+
+def total_salary(path):
+    return
+
+total, average = total_salary("path/to/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
