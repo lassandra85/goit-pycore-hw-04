282b4152e82cd2c7bc53c69e74816f556daea97c
lassandra85 <lassandra85@gmail.com>
2024-10-30 16:19:14 +0100

    1

--- tk1.py
+++ tk1.py
@@ -1,6 +1,33 @@
+from pathlib import Path
 
 def total_salary(path):
-    return
 
-total, average = total_salary("path/to/salary_file.txt")
+    file_path = Path(path)
+
+    if file_path.exists():
+
+        text = file_path.read_text(encoding="utf-8")
+        total = 0
+        count = 0
+
+        for line in text:
+            try:
+                name, salary = line.strip().split(",")
+                total += int(salary)
+                count += 1
+            except ValueError:
+                print("Файл містить помилкові дані. Перевірте вміст.")
+
+                return 0, 0
+            
+        average = total / count if count > 0 else 0
+
+        return total, average
+    else:
+        print("Файл не знайдено. Перевірте шлях до файлу.")
+        return 0, 0
+   
+
+# Приклад використання функції
+total, average = total_salary('path/to/salary_file.txt')
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
